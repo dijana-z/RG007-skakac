5e7afc592bd6fe6adf1bbafc4d5db369fd843cdc
Dijana Zulfikaric <dijanaz1996@gmail.com>
2018-04-20 01:51:06 +0200

    implemented basic moves

--- callbacks.c
+++ callbacks.c
@@ -1,172 +1,180 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <GL/glut.h>
 #include "callbacks.h"
+#include "functions.h"
 
 #define SPACE 32
 #define ESC 27
+#define MAX_KEYS 256
+
+#define TIMER_INT 10
+#define TIMER0_ID 0
+#define TIMER1_ID 1
 
 /* camera position parameters */
-static float cam_position_x = 0;
-static float cam_position_y = 0;
-static float cam_position_z = 10;
+float cam_eye_x = 0, cam_eye_y = 0, cam_eye_z = 10;
+
+int key_pressed[256];
 
 /* jumping parameters */
-static float translate_y = 0.1;
-static float move_y = 4;
+float original_y = 0;
+float translate_x = 0, translate_y = 0;
+float move_y = 0.001, move_x = 10;
 
 /* size of the player */
-static float cube_size = 40;
-
-/* gravity parameter */
-static float gravity = 0.4;
+float cube_size = 40;
 
 /* rotation parameter */
-static float param = 10;
+float angle_param = 10;
 
-/* static window width and height sizes */
-static float window_width;
-static float window_height;
+/*  window width and height sizes */
+float window_width, window_height;
 
 /* animation parameters */
-static int start_animation = 0;
-static int jump_up = 0;
+int start_animation = 0;
+int jump_up = 0;
 
-/* light parameters */
-GLfloat light_position[] = { 1, 1, 1, 0 };
-GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1 };
-GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
-GLfloat light_specular[] = { 0.9, 0.9, 0.9, 1 };
-GLfloat ambient_coeffs[] = { 1.0, 0.1, 0.1, 1 };
-GLfloat diffuse_coeffs[] = { 0.0, 0.0, 0.8, 1 };
-GLfloat specular_coeffs[] = { 1, 1, 1, 1 };
-GLfloat shininess = 40;
 
 void on_keyboard(unsigned char key, int x, int y)
 {
-  (void)x;
-  (void)y;
+    (void)x;
+    (void)y;
 
-  switch (key) {
+    switch (key) {
     /* exit the game */
-    case ESC:
-      exit(0);
-      break;
-    /* start the game */
-    case SPACE:
-      if(!start_animation){
-        start_animation = 1;
-        glutTimerFunc(10, on_timer, 0);
-      }
-      break;
-    /* jump up */
-    case 'W':
-    case 'w':
-      if(start_animation){
-        if(!jump_up){
-        jump_up = 1;
-        jumpUp();
-        }
-      }
-      break;
-  }
+        case ESC:
+            exit(EXIT_SUCCESS);
+            break;
+        /* start or stop the game */
+        case SPACE:
+            if(!start_animation){
+                start_animation = 1;
+                glutTimerFunc(TIMER_INT, on_timer, TIMER0_ID);
+            } else {
+                start_animation = 0;
+            }
+            break;
+        /* jump up */
+        case 'W':
+        case 'w':
+            if(start_animation){
+                if(!jump_up){
+                    jump_up = 1;
+                    glutTimerFunc(TIMER_INT, on_timer_jumping, TIMER1_ID);
+                }
+            }
+            break;
+        /* go left */
+        case 'a':
+        case 'A':
+            if(start_animation){
+                key_pressed['a'] = 1;
+                glutTimerFunc(TIMER_INT, on_timer, TIMER0_ID);
+            }
+            break;
+        /* go right */
+        case 'd':
+        case 'D':
+            if(start_animation){
+                key_pressed['d'] = 1;
+                glutTimerFunc(TIMER_INT, on_timer, TIMER0_ID);
+            }
+            break;
+    }
+}
+
+void on_key_release(unsigned char key, int x, int y)
+{
+    (void)x;
+    (void)y;
+
+    switch (key) {
+        case 'a':
+        case 'A':
+            key_pressed['a'] = 0;
+            break;
+        case 'd':
+        case 'D':
+            key_pressed['d'] = 0;
+                break;
+    }
 }
 
 void on_reshape(int width, int height)
 {
-  window_height = height;
-  window_width = width;
+    window_height = height;
+    window_width = width;
 
-  glViewport(0, 0, width, height);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-width/2, width/2, -height/2, height/2, -100, 100);
-  glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-width/2, width/2, -height/2, height/2, -100, 100);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void on_timer(int value)
 {
-  if(value != 0){
-    return;
-  }
-  glutPostRedisplay();
+    if(value != TIMER0_ID){
+        return;
+    }
 
-  if(jump_up) {
-    jumpUp();
-  }
-  if(start_animation){
-    start_animation = 1;
-    glutTimerFunc(10, on_timer, 0);
-  }
+    glutPostRedisplay();
+
+    if(start_animation){
+        start_animation = 1;
+        glutTimerFunc(TIMER_INT, on_timer, TIMER0_ID);
+    }
+}
+
+void on_timer_jumping(int value)
+{
+    if(value != TIMER1_ID) {
+        return;
+    }
+
+ /* increment the rotation parameter */
+    angle_param += 5%360;
+
+    if(jump_up){
+        translate_y = 110*sin(move_y);
+        move_y += 0.05;
+
+        if(move_y >= 3.14){
+            move_y = 0;
+        }
+
+        if(translate_y <= original_y){
+            move_y = 0.001;
+            jump_up = 0;
+        }
+    }
+
+    if(jump_up){
+        glutTimerFunc(TIMER_INT, on_timer_jumping, TIMER1_ID);
+    }
 }
 
 void on_display(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glLoadIdentity();
-  gluLookAt(
-    cam_position_x, cam_position_y, cam_position_z,
-    0, 0, 0,
-    0, 1, 0
-  );
+    glLoadIdentity();
+    gluLookAt(
+        cam_eye_x, cam_eye_y, cam_eye_z,
+        0, 0, 0,
+        0, 1, 0
+    );
 
-  initLighting();
+    initLighting();
 
-  glTranslatef(0, translate_y, 0);
-  glRotatef(param, 0, 1, 0);
+    glPushMatrix();
+    glTranslatef(translate_x, translate_y, 0);
+    glRotatef(angle_param, 0, 1, 0);
+    move();
+    drawPlayer();
+    glPopMatrix();
 
-  glPushMatrix();
-  drawPlayer();
-  glPopMatrix();
-
-  glutSwapBuffers();
-}
-
-void initLighting(void)
-{
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-
-  glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
-  glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
-  glMaterialf(GL_FRONT, GL_SHININESS, shininess);
-}
-
-void drawPlayer(void)
-{
-  glColor3f(1, 0, 0);
-  glTranslatef(0, -window_height/2+cube_size/1.5, 0);
-  glutSolidCube(40);
-}
-
-void jumpUp(void)
-{
-  /* change the rotation parameter */
-  param += 5%360;
-
-  printf("%f\n", translate_y);
-  if(translate_y > 50){
-    move_y = -move_y;
-    translate_y += move_y - gravity;
-    move_y -= 1;
-  }
-  if(translate_y < 50){
-    translate_y += move_y - gravity;
-    move_y += 1;
-  }
-
-  if(translate_y <= 0){
-    jump_up = 0;
-    move_y = 4;
-    translate_y = 0.1;
-  }
-
-  if(start_animation){
-    start_animation = 1;
-    glutTimerFunc(10, on_timer, 0);
-  }
+    glutSwapBuffers();
 }
--- callbacks.h
+++ callbacks.h
@@ -1,10 +1,8 @@
 #pragma once
 
 void on_keyboard(unsigned char key, int x, int y);
+void on_key_release(unsigned char key, int x, int y);
 void on_display(void);
 void on_reshape(int width, int height);
 void on_timer(int value);
-
-void initLighting(void);
-void drawPlayer(void);
-void jumpUp(void);
+void on_timer_jumping(int value);
