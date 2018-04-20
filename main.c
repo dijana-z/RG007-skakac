ed11094c40604ceb2addd79a1aa084a7420e3308
Dijana Zulfikaric <dijanaz1996@gmail.com>
2018-04-20 19:03:33 +0200

    initial coordinates

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <time.h>
 #include "callbacks.h"
+#include "functions.h"
 
 int main(int argc, char** argv)
 {
@@ -21,16 +23,19 @@ int main(int argc, char** argv)
     /* clear window */
     glClearColor(0, 0, 0, 0);
 
+    srand(time(NULL));
+
     /* disabling the key repeat */
     glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
 
     glShadeModel(GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_NORMALIZE);
-	glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_COLOR_MATERIAL);
 
+    initCoordinats();
     /* start the main loop */
     glutMainLoop();
 
